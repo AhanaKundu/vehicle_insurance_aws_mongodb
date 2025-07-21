a582c80648dddbb5014bf36e3c53380ccd121734
AhanaKundu <ahana.job@gmail.com>
2025-07-21 12:14:28 +0530

    project structure, mongodb dataloading, logging, exception done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ahana Kundu",
+    author_email="ahana.job@gmail.com",
+    packages=find_packages()
+)
